e6396f186ee69e59fb2e12be6440e981ce410b37
azizul104 <96073261+azizul104@users.noreply.github.com>
2021-12-22 21:53:03 +0600

    Create 1A.cpp

---
+++ 1A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m, a;
+    long long x, y, sum;
+    cin >> n >> m >> a;
+    x = n / a;
+    y = m / a;
+    if (n % a != 0) {
+        x = x + 1;
+    }
+    if (m % a != 0) {
+        y = y + 1;
+    }
+    sum = x * y;
+    cout << sum;
+}
